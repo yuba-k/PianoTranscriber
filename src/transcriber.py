f0ea0cd1186b3c9185cbd930366d7beb746ba015
yubak <yubakei062318@outlook.jp>
2024-12-25 21:08:27 +0900

    add , update : Add a new function to estimate the scale and the duration of notes

--- src/transcriber.py
+++ src/transcriber.py
@@ -4,39 +4,48 @@ import wave
 import librosa
 import numpy as np
 from librosa import display
-
+import noisereduce as nr
 
 class Scale_Identification():
     def __init__(self):
         self.NoteNameList = {
-            0:"C",
-            1:"C#",
-            2:"D",
-            3:"D#",
-            4:"E",
-            5:"F",
-            6:"F#",
-            7:"G",
-            8:"G#",
-            9:"A",
-            10:"A#",
-            11:"B",
+            # 0:"C",
+            # 1:"C#",
+            # 2:"D",
+            # 3:"D#",
+            # 4:"E",
+            # 5:"F",
+            # 6:"F#",
+            # 7:"G",
+            # 8:"G#",
+            # 9:"A",
+            # 10:"A#",
+            # 11:"B",
+            0:"ド",
+            1:"ド#",
+            2:"レ",
+            3:"レ#",
+            4:"ミ",
+            5:"ファ",
+            6:"ファ#",
+            7:"ソ",
+            8:"ソ#",
+            9:"ラ",
+            10:"ラ#",
+            11:"シ",
         }
-        self.path = "../music_sources/myscore/"
-        self.filename = [f for f in os.listdir(self.path) if os.path.isfile(os.path.join(self.path,f))]
+        self.path = "../music_sources/myscore/edelweiss.wav"
 
     def ReadFile(self):
-        for i in self.filename:
-            waveFile = wave.open(self.path + i)
-            wav , sr = librosa.load(self.path + i,sr=waveFile.getframerate())
-            chromagram = librosa.feature.chroma_stft(y=wav,sr=sr)
-            result = self.Identification(self.Molding(chromagram))
-            print(result)
+        waveFile = wave.open(self.path)
+        self.wav , self.sr = librosa.load(self.path,sr=waveFile.getframerate(),mono=True)
 
     def Molding(self,temp):
         return np.fliplr(temp.T)
 
-    def Identification(self,chrom):
+    def NoteIdentification(self):
+        chromagram = librosa.feature.chroma_stft(y=self.wav,sr=self.sr)
+        chrom = self.Molding(chromagram)
         chrom = np.array(chrom)
         ResultNote = []
         max_index = len(chrom) #cheak the size of list
@@ -44,10 +53,51 @@ class Scale_Identification():
             index = int(np.where(chrom[i] == 1)[0])
             ResultNote.append(self.NoteNameList[11-index])
         return ResultNote
+
+    def NoteLenghtIndetification(self):
+        wav_denoised = nr.reduce_noise(y=self.wav, sr=self.sr)
+
+        onset_env = librosa.onset.onset_strength(y=wav_denoised, sr=self.sr)
+
+        tempo, _ = librosa.beat.beat_track(onset_envelope=onset_env, sr=self.sr)
+
+        quarter = 60/tempo
+
+        self.notelength = {
+            "whole":quarter*4,
+            "half":quarter*2,
+            "quarter":quarter,
+            "eighth":quarter/2,
+            "sixteenth":quarter/4
+        }
+        print(len(onset_env))
+        return self.notelength
+
+    def a(self,note):
+        temp = []
+        ignoreframe = self.notelength["sixteenth"]*self.sr/512
+        print(ignoreframe)
+        j = 0
+        for i in range(1,len(note)-1):
+            if (note[i-1] == note[i]):
+                j = j + 1
+            elif (note[i-1] != note[i]):
+                if(j > ignoreframe):
+                    temp.append(note[i-1])
+                    j = 0
+        if(j > ignoreframe):
+            temp.append(note[len(note)-1])
+        return temp
         
 def main():
     scaledefine = Scale_Identification()
     scaledefine.ReadFile()
+    result1 = scaledefine.NoteIdentification()
+    print(result1)
+    result2 = scaledefine.NoteLenghtIndetification()
+    print(result)
+    temp = scaledefine.a(result1)
+    print(temp)
 
 if __name__ == "__main__":
     main()
